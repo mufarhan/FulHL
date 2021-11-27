71001d7883735734eb0abf8d32e9423665b638d0
Muhammad Farhan <mufarhan.pervaiz@gmail.com>
2021-11-28 02:15:29 +1100

    Update main.cpp

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -14,25 +14,26 @@ int main(int argc, char **argv) {
     hl.SelectLandmarks_HD(topk);
 
     if(string(argv[1]).compare("construct_labelling") == 0) {
-
       cout << "Constructing Highway Cover Labelling..." << std::endl;
       hl.BuildIndex(topk);
+      
       hl.StoreLabelling(argv[4]); //storing labelling to disk
-
       hl.deallocate(0);
     } else if(string(argv[1]).compare("update_labelling") == 0) {
       hl.LoadLabelling_Full(argv[4], topk, k); //loading labelling from disk
 
       cout << "Updating Highway Cover Labelling..." << std::endl;
       hl.UpdateLabelling(argv[5], atoi(argv[6]));
+      
       hl.StoreLabelling(argv[4]); //storing labelling to disk after update
-
       hl.deallocate(0);
     } else if (string(argv[1]).compare("query_labelling") == 0) {
       hl.LoadLabelling_Pruned(argv[4]); //loading labelling from disk
-
       hl.RemoveLandmarks(topk);
+      
+      cout << "Performing Distance Queries..." << std::endl;
       hl.QueryDistance(argv[5], argv[6]);
+      
       hl.deallocate(1);
     }
   }
