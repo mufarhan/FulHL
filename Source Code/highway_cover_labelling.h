8bb32110ee155583ab69e945e5ada6d8c678e0c4
Muhammad Farhan <mufarhan.pervaiz@gmail.com>
2022-07-11 13:16:39 +1000

    Update highway_cover_labelling.h

--- Source Code/highway_cover_labelling.h
+++ Source Code/highway_cover_labelling.h
@@ -20,7 +20,7 @@
 
 class HighwayLabelling {
  public:
-  // Constructs an index from a graph, given as a list of edges.
+  // Constructs labelling for a graph, given as a list of edges.
   HighwayLabelling(std::string filename, int k);
   HighwayLabelling();
   ~HighwayLabelling();
